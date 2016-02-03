81f74963083846393480240c876862ee68cbde8c
Gunther Piez <gupiez@gmail.com>
2016-02-03 14:19:37 +0100

    Major refactoring of characters into armory

---
+++ achievementindex.cpp
@@ -0,0 +1,6 @@
+#include "achievementindex.h"
+
+AchievementIndex::AchievementIndex()
+{
+
+}
---
+++ achievementindex.h
@@ -0,0 +1,11 @@
+#ifndef ACHIEVEMENTINDEX_H
+#define ACHIEVEMENTINDEX_H
+
+
+class AchievementIndex
+{
+public:
+    AchievementIndex();
+};
+
+#endif // ACHIEVEMENTINDEX_H
